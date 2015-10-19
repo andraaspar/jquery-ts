025e9c088fadafbc106739a2f368fe96146f2a3d
andraaspar <andraaspar@gmail.com>
2015-10-19 12:22:27 +0200

    Updated IAJAXSettings: type out, method in.

--- src/jQuery/IAJAXSettings.ts
+++ src/jQuery/IAJAXSettings.ts
@@ -28,6 +28,7 @@ module jQuery {
 		isLocal?: boolean;
 		jsonp?: string;
 		jsonpCallback?: any;
+		method?: string;
 		mimeType?: string;
 		password?: string;
 		processData?: boolean;
@@ -36,7 +37,6 @@ module jQuery {
 		success?: IXHRDoneFunction;
 		timeout?: number;
 		traditional?: boolean;
-		type?: string;
 		url?: string;
 		username?: string;
 		xhr?: IAJAXSettingsXHRFunction;
